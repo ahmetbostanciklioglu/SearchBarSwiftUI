cb695293a50de4012ecc851b9f954ce2f01fcb8f
Ahmet <ahmetbostancikli@gmail.com>
2025-12-03 13:34:48 +0300

    List Filtering is added with SearchBar in SwiftUI

--- SearchBarSwiftUI/SearchBar.swift
+++ SearchBarSwiftUI/SearchBar.swift
@@ -1,50 +1,27 @@
 import SwiftUI
+import Combine
 
 struct SearchBar: View {
-    @State private var searchText: String = ""
+    @StateObject private var viewModel = ViewModel()
     var body: some View {
-        SearchView(searchText: $searchText)
+        VStack {
+            SearchView(searchText: $viewModel.searchText)
+             
+            List(viewModel.filteredData, id: \.self) {
+                Text($0)
+            }
+        }
     }
 }
 
-/// UIViewRepresentable is a Wrapper between view(UIKit i.e. UISearchBar) and SwiftUI
-struct SearchView: UIViewRepresentable {
-    /// We use searchText for data connection with SwiftUI
-    @Binding var searchText: String
+
+class ViewModel: ObservableObject {
+    @Published var datas = ["hello", "World", "Swift", "UI"]
+    @Published var searchText: String = ""
     
-    /// Creating a UISearchBar View
-    func makeUIView(context: UIViewRepresentableContext<SearchView>) -> UISearchBar {
-        let searchbar = UISearchBar()
-        searchbar.barStyle = .default
-        searchbar.placeholder = "Write here..."
-        searchbar.autocapitalizationType = .none
-        
-        searchbar.delegate = context.coordinator
-        return searchbar
-    }
-    
-    /// Connecting uiView (UISearchBar) and searchText (SwiftUI string)
-    func updateUIView(_ uiView: UISearchBar, context: Context) {
-        uiView.text = searchText
-    }
-    
-    /// To use UIKit Delegate methods we make Coordinator to connect with SearchView
-    func makeCoordinator() -> Coordinator {
-        return SearchView.Coordinator(parent1: self)
-    }
-    
-    /// Bridging UIKit  searchView to SwiftUI searchView with parent.searchText = searchText
-    class Coordinator: NSObject, UISearchBarDelegate {
-        var parent: SearchView!
-        
-        init(parent1: SearchView) {
-            parent = parent1
-        }
-        
-        /// To listen text change we use this Delegate method
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            parent.searchText = searchText
-        }
+    /// Filtering the datas at search time and according to that showing the items in the list.
+    var filteredData: [String] {
+        datas.filter { searchText.isEmpty ? true : $0.localizedCaseInsensitiveContains(searchText)  }
     }
 }
 
