e7cc95ef7defd51691f25de1f31c803bd6b36154
Ahmet <ahmetbostancikli@gmail.com>
2025-12-03 12:50:32 +0300

    Initial Commit

---
+++ SearchBarSwiftUI/SearchBar.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+struct SearchBar: View {
+    @State private var searchText: String = ""
+    var body: some View {
+        SearchView(searchText: $searchText)
+    }
+}
+
+/// UIViewRepresentable is a Wrapper between view(UIKit i.e. UISearchBar) and SwiftUI
+struct SearchView: UIViewRepresentable {
+    /// We use searchText for data connection with SwiftUI
+    @Binding var searchText: String
+    
+    /// Creating a UISearchBar View
+    func makeUIView(context: UIViewRepresentableContext<SearchView>) -> UISearchBar {
+        let searchbar = UISearchBar()
+        searchbar.barStyle = .default
+        searchbar.placeholder = "Write here..."
+        searchbar.autocapitalizationType = .none
+        
+        searchbar.delegate = context.coordinator
+        return searchbar
+    }
+    
+    /// Connecting uiView (UISearchBar) and searchText (SwiftUI string)
+    func updateUIView(_ uiView: UISearchBar, context: Context) {
+        uiView.text = searchText
+    }
+    
+    /// To use UIKit Delegate methods we make Coordinator to connect with SearchView
+    func makeCoordinator() -> Coordinator {
+        return SearchView.Coordinator(parent1: self)
+    }
+    
+    /// Bridging UIKit  searchView to SwiftUI searchView with parent.searchText = searchText
+    class Coordinator: NSObject, UISearchBarDelegate {
+        var parent: SearchView!
+        
+        init(parent1: SearchView) {
+            parent = parent1
+        }
+        
+        /// To listen text change we use this Delegate method
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            parent.searchText = searchText
+        }
+    }
+}
+
+#Preview("Search Bar View Preview") {
+    SearchBar()
+}
